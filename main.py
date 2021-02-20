2ab3072967dfbe24026415be923405e9aa2a2493
kykesus <omerber95@gmail.com>
2021-02-21 01:18:18 +0200

    added comments as placeholders + added requirements file

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 #!/usr/bin/python
 
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import ConcatDataset, DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
+
 
 def main():
 
     # (Down)load MNIST dataset
-    # We use tensor transfrom 
-    dataset_train = MNIST('./data/', train=True, download=True, transform=ToTensor())
-    dataset_test = MNIST('./data/', train=False, download=True, transform=ToTensor())
+    # We use tensor transform
+    dataset_train = MNIST('./data/', train=True,
+                          download=True, transform=ToTensor())
+    dataset_test = MNIST('./data/', train=False,
+                         download=True, transform=ToTensor())
+
+    # Merge train & test
+    dataset = ConcatDataset([dataset_train, dataset_test])
 
     # Load the samples to memory
-    data_train = DataLoader(dataset_train)
-    data_test = DataLoader(dataset_test)
+    # TODO - should we shuffle?
+    data = DataLoader(dataset, batch_size=len(dataset))
 
-    # Merge train & test data
-    data = ?
-    labels = ?
+    # Apply preprocessing - we decided to use PCA
+    # This preprocessing keeps the input for the next stage convex
+    ###
+    # Preprocessing
+    ###
+
+    # Define the binary classification problems
+
+    # Determine the loss function to work with
+
+    # Create true labels for the problems
+
+    # Discuss and definitions for different gradients
+
+    # Part A - Optimization
+    # Run learning, plot figures and whatever...
+
+    # Part B - Generalization
+    # Use randomized train & test sets
+
 
 if __name__ == "__main__":
     main()
